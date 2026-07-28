c647135ca8a8f64326039cac94aa42778fa56339
Hazem <hazem77mostafa90@gmail.com>
2026-07-28 17:46:51 +0300

    customoization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <p className=" text-2xl text-red-500">Hello World</p>
+    <div className="flex flex-col gap-y-4 p-4">
+      <div>
+        <Button variant="elevated">I am a Button</Button>
+      </div>
+      <div>
+        <Input placeholder="I am an Input" />
+      </div>
+      <div>
+        <Progress value={75} />
+      </div>
+      <div>
+        <Textarea placeholder="I am a Textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
